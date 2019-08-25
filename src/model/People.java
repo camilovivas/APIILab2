6d81f687b07ea817fd812a0395556f696dd418e4
camilo <camilo-152000@hotmail.com>
2019-08-25 15:11:53 -0500

    se crean clases

---
+++ src/model/People.java
@@ -0,0 +1,7 @@
+package model;
+
+import java.io.Serializable;
+
+public class People implements Serializable{
+
+}
---
+++ test/InvestorTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class InvestorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
