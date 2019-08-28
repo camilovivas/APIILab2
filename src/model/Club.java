5761bc17bf9dc40aa061422994de100e9fcaf839
camilovivas <47890754+camilovivas@users.noreply.github.com>
2019-08-28 17:55:19 -0500

    se hace cargador de persona

--- src/model/Club.java
+++ src/model/Club.java
@@ -1,6 +1,10 @@
 package model;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -19,8 +23,26 @@ public class Club {
 		owners = new ArrayList<People>();
 	}
 	
-	public void chargePeople() {
+	public void chargePeople() throws FileNotFoundException {
+			String persona = "";
 		File archive = new File("C:");
+		try {
+			BufferedReader c = new BufferedReader(new FileReader(archive));
+			StringBuffer d = new StringBuffer();
+			String texto;
+			while((texto = c.readLine()) != null){
+				persona += c.toString();
+				String [] camposPersonas = persona.split(",");
+				People ensayo = new People(camposPersonas[0], camposPersonas[1], camposPersonas[2], camposPersonas[3] );
+				owners.add(ensayo);
+			}
+			
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
 		
